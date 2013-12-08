7c329d1890be90b9678f9f85c3e398d4d46c3001
Rahul Malviya <malviyarahul2001@gmail.com>
2013-12-07 18:45:19 -0800

    Adding sample for logging in logIn.java

--- src/goair/servlets/general/logIn.java
+++ src/goair/servlets/general/logIn.java
@@ -16,64 +16,57 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
+import org.apache.log4j.Logger;
 
 /**
  * Servlet implementation class logIn
  */
 public class logIn extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public logIn() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	private static Logger sLogger = Logger.getLogger(logIn.class);
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public logIn() {
+		super();
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 		AdminServicesProxy adminProxy = new AdminServicesProxy();
 		AirlineServicesProxy airlineProxy = new AirlineServicesProxy();
 		CustomerServicesProxy customerProxy = new CustomerServicesProxy();
 		EmployeeServicesProxy employeeProxy = new EmployeeServicesProxy();
-		
+
 		String userName = request.getParameter("email");
 		String password = request.getParameter("password");
-		
-	
-		
-		PrintWriter out = response.getWriter();	
+
+		sLogger.info("Checking login for username : " + userName + ", password : " + password);
 		employeeProxy.setEndpoint("http://localhost:8080/goAir/services/EmployeeServices");
-		
 		HttpSession session = request.getSession(false);
 		String role = (String) session.getAttribute("role");
 		Employee emp = null ;
-		
-		//write a db query to get role from employee table
-		
 		if(role.contains("Admin"))
 		{
-			 emp  = employeeProxy.employeeLogin(userName, password);
+			emp  = employeeProxy.employeeLogin(userName, password);
+			if(emp != null) {
+				sLogger.info("Valid admin.");
+			}
+			else {
+				sLogger.info("Invalid user/password for admin.");
+			}
 		}
-		 
-		  
-		
-		 //request.setAttribute("employee",emp);
-		 RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/View/GeneralView/welcome.jsp");
-		 dispatcher.forward(request, response);
-		
-	}
 
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/View/GeneralView/welcome.jsp");
+		dispatcher.forward(request, response);
+	}
 }
